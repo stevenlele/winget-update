13b91fd70a571d64326deea01f23a983c0e877c5
stevenlele <stevenlele@outlook.com>
2024-08-15 21:13:19 +0000

    Initial commit

---
+++ common.py
@@ -0,0 +1,16 @@
+import subprocess
+from sys import stderr, stdout
+
+
+def get(url: str):
+    return subprocess.run(
+        ("curl", url), check=True, stdout=subprocess.PIPE, stderr=stderr
+    ).stdout.decode()
+
+
+def run_komac(identifier: str, version: str, url: str, base_version: str = ""):
+    command = ["./komac", identifier, "-v", version, "-u", url]
+    if base_version:
+        command.extend(("--base-version", base_version))
+    print("$", subprocess.list2cmdline(command), flush=True)
+    subprocess.run(command, check=True, stdout=stdout, stderr=stderr)
---
+++ discord.py
@@ -0,0 +1,26 @@
+import json
+
+from common import *
+
+
+def main():
+    with open("discord.txt") as f:
+        x64, x86 = map(int, f.read().split(","))
+        versions = {"x64": x64, "x86": x86}
+
+    for arch, old_version in versions.items():
+        url = f"https://updates.discord.com/distributions/app/manifests/latest?channel=stable&platform=win&arch={arch}"
+        triple: list[int] = json.loads(get(url))["full"]["host_version"]
+        assert triple[:2] == [1, 0]
+        assert (new_version := triple[3]) >= old_version
+        if new_version > old_version:
+            run_komac(
+                "Discord.Discord",
+                f"1.0.{new_version}",
+                f"https://dl.discordapp.net/distro/app/stable/win/{arch}/1.0.{new_version}/DiscordSetup.exe",
+                f"1.0.{old_version}",
+            )
+            versions[arch] = new_version
+
+    with open("discord.txt", "w") as f:
+        f.write(f"{versions['x64']},{versions['x86']}")
