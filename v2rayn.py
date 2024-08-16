dad96658f985bb8e8038cf93a6eaae87f6fb5c13
stevenlele <stevenlele@outlook.com>
2024-08-16 12:04:57 +0000

    Tweak

--- v2rayn.py
+++ v2rayn.py
@@ -10,32 +10,34 @@ def main():
 
     release = json.loads(get("https://api.github.com/repos/2dust/v2rayN/releases"))[0]
 
-    assert (new_version := Version(release["tag_name"])) >= old_version
+    if (new_version := Version(release["tag_name"])) == old_version:
+        return
 
-    if new_version > old_version:
-        url = next(
-            asset["browser_download_url"]
-            for asset in release["assets"]
-            if asset["name"] == "v2rayN-With-Core.zip"
-        )
+    assert new_version > old_version
 
-        release_notes: str = release["body"]
-        release_notes = re.sub(r"^#+ 本次更新.*", "", release_notes)
-        release_notes = re.sub(r"#+ 注意.+", "", release_notes, flags=re.DOTALL)
-        release_notes = release_notes.strip()
+    url = next(
+        asset["browser_download_url"]
+        for asset in release["assets"]
+        if asset["name"] == "v2rayN-With-Core.zip"
+    )
 
-        run_komac(
-            "2dust.v2rayN",
-            f"{new_version}",
-            url,
-            {
-                "owner": "2dust",
-                "repo": "v2rayN",
-                "release_notes": release_notes,
-                "release_notes_url": release["html_url"],
-                "release_notes_locale": "zh-CN",
-            },
-        )
+    release_notes: str = release["body"]
+    release_notes = re.sub(r"^#+ 本次更新.*", "", release_notes)
+    release_notes = re.sub(r"#+ 注意.+", "", release_notes, flags=re.DOTALL)
+    release_notes = release_notes.strip()
 
-        with open("v2rayn.txt", "w") as f:
-            f.write(str(new_version))
+    run_komac(
+        "2dust.v2rayN",
+        f"{new_version}",
+        url,
+        {
+            "owner": "2dust",
+            "repo": "v2rayN",
+            "release_notes": release_notes,
+            "release_notes_url": release["html_url"],
+            "release_notes_locale": "zh-CN",
+        },
+    )
+
+    with open("v2rayn.txt", "w") as f:
+        f.write(str(new_version))
